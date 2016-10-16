f7fd68d5b3fa7523f986cda6d71fa1fac6e1f1a8
Robert Strand <RobertS@eostar.com>
2016-10-16 15:21:48 -0400

    resources

---
+++ XFURecap/XFURecap/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XFURecap
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+		}
+
+
+		void Button_OnClicked(object sender, EventArgs e)
+		{
+			Resources["fgColor"] = Color.FromHex("#C0C0C0");
+			Resources["bgColor"] = Color.FromHex("#606060");
+		}
+	}
+}
